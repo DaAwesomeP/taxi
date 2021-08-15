f5d96614e0c3c61bd36082f7fd769db0e4dcfc01
Perry Naseck <git@perrynaseck.com>
2021-08-15 11:47:50 -0400

    auto build and publish wheels, ready for PyPi

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ import setuptools
 readme = (pathlib.Path(__file__).parent / "README.md").read_text(encoding="utf-8")
 
 setuptools.setup(
-    name="taxi",
-    version="0.1.0a0",
+    name="axidraw-taxi",
     description="Opinionated Axidraw interface",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -15,16 +14,19 @@ setuptools.setup(
     url="https://github.com/plottertools/taxi",
     license="MIT",
     packages=setuptools.find_packages(exclude=["tests"]),
-    python_requires=">=3.8,<3.10",
+    python_requires=">=3.7,<3.10",
     package_data={"taxi": ["*.kv", "*.json"]},
     install_requires=[
-        "numpy>=1.19",
-        "setuptools",
-        "kivy>=2.0.0",
-        "vpype>=1.7.0",
+        "kivy>=2.0.0,<3",
+        "vpype>=1.7.0,<2",
         "pyaxidraw @ https://cdn.evilmadscientist.com/dl/ad/public/AxiDraw_API.zip",
-        "watchgod>=0.7",
-        "aiohttp",
-        "Pillow",
+        "watchgod>=0.7,<1",
+        "aiohttp>=3,<4",
     ],
+    extras_require={
+        "dev": [
+            "black>=21,<22",
+            "isort>=5,<6",
+        ]
+    }
 )
